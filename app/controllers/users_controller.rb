e67e7a327d5018017ed254e3911eb502208c8243
Babar Al-Amin <knock@babar.im>
2020-10-15 23:12:29 +0200

    Add User#create action to find valid email address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    user = User.find_or_initialize_by(user_params)
+    unless user.persisted?
+      user.email = ValidEmailFinder.new(user_params[:first_name], user_params[:last_name], user_params[:domain]).find!
+      user.save
+    end
+
+    if user.persisted?
+      redirect_to user_path(user), notice: "Email address found!"
+    else
+      redirect_to root_path, notice: "Could not determine a valid email address!"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :domain)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :domain
+  validates_presence_of :email
   validates_uniqueness_of :email
 end
